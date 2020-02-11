ebfadaa862a972fd2ae3d6bb77f452721d5060ad
keitoku.ikegami <keitoku.ikegami2020@klas.ch>
2020-02-11 15:17:15 +0100

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by IKEGAMIKeitoku on 11.02.20.
+//  Copyright © 2020 keitoku.ikegami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
